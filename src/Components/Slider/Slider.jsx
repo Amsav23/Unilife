9bd09d85c9be1a476425e26091416854d1bfdf2c
Amsav23 <130859471+Amsav23@users.noreply.github.com>
2023-09-12 16:48:24 -0400

    Updated Slider.jsx
    Got the Search by City search bar to work, or at least identify cities in the Console.

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,12 +1,42 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import './Slider.css'
-import background from '../../assets/cover-img.png';
+import background from '../../assets/cover-img.png'
+import SearchIcon from '../../assets/Search-icon.png'
+import CompareIcon from '../../assets/Compare-icon.png'
+import BillsIcon from '../../assets/Bills-Included-icon.png'
 import axios from 'axios';
 
-function Slider() {
-  const [topCities, setTopCities] = useState([]);
+function Search({setTopCities}) {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(query)
+
+    //make API call to filter
+    axios.get(`https://unilife-server.herokuapp.com/cities/?name=${query}`)
+    .then(res =>{
+      console.log(res.data.response)
+      setTopCities(res.data.response)
+    })
 
 
+    .catch(err => {
+      if (err.response.status === 404){
+        alert(`There is no city named ${query}`)
+      }
+      else{
+        console.log(err)
+      }
+    })
+
+    //clear textbox
+    setQuery('')
+  }
+
+
+
+/*
   useEffect(()=>{
     axios(
       `${import.meta.env.VITE_API_BASE_URL}`)
@@ -18,8 +48,9 @@ function Slider() {
 
     .catch((err) => console.log(err));
   }, []);
+*/
 
-
+/*
   const sliderStyle = {
     backgroundImage: `url(${import.meta.env.VITE_API_BASE_URL}${topCities[0]})`,
     backgroundSize: "cover",
@@ -30,17 +61,27 @@ function Slider() {
     position: "relative",
     zIndex: 0
   };
-
+*/
 
 
   return (
-    <div style={sliderStyle}>Slider</div>
-
-    /*
     <div className="slider-container">
-        <div style={{ backgroundImage: `url(${background})` }}></div>
-    </div>*/
+      <h1>Slider</h1>
+      <img src={background} alt="background image"></img>
+
+    <form className="search-container" onSubmit={handleSubmit}>
+        <input type="text" value={query}
+               placeholder="Search by city"
+               onChange={(e)=>setQuery(e.target.value)} />
+
+    </form>
+
+      <img src={SearchIcon} alt="Search-icon"></img>
+      <img src={CompareIcon} alt="Compare-icon"></img>
+      <img src={BillsIcon} alt="Bills-icon"></img>
+
+    </div>
   )
 }
 
-export default Slider
+export default Search
